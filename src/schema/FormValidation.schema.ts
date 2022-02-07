b3479b050c043a7827dd27674ca60e190839ddf2
luiza <l.davtyan@teamforce.dev>
2022-02-07 12:16:30 +0400

    yup typo fix

--- src/schema/FormValidation.schema.ts
+++ src/schema/FormValidation.schema.ts
@@ -3,10 +3,10 @@ import * as Yup from 'yup';
 export const FormValidationSchema = Yup.object().shape({
   title: Yup.string()
     .required('Title is required')
-    .min(1, 'Title must be at least 6 characters')
-    .max(1000, 'Title must not exceed 20 characters'),
+    .min(1, 'Title must be at least 1 characters')
+    .max(1000, 'Title must not exceed 1000 characters'),
   body: Yup.string()
     .required('Body is required')
-    .min(1, 'Body must be at least 6 characters')
-    .max(1000, 'Body must not exceed 40 characters'),
+    .min(1, 'Body must be at least 1 characters')
+    .max(1000, 'Body must not exceed 1000 characters'),
 });
